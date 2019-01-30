d28dfb77790e8ae5d2948ff0a2875132086014c7
github username <github email address>
2019-01-29 17:27:26 -0800

    practice building classes, setter/getter macros and instance methods

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :size, :color, :genre, :material, :condition
+  attr_reader :brand
+
+  def initialize(shoeBrand)
+    @brand = shoeBrand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
+  end
+
+
+end
